4e93235e8dc854cf9a1e6c3440b2674b32ee687e
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-01-25 15:19:36 +1100

    Add shortcut

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import { register } from '@tauri-apps/plugin-global-shortcut';
 
 const greetMsg = ref("");
 const name = ref("");
@@ -9,6 +10,10 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsg.value = await invoke("greet", { name: name.value });
 }
+
+register('CommandOrControl+Shift+L', () => {
+  console.log('Shortcut triggered');
+});
 </script>
 
 <template>
