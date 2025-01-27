30ebbc49de09a92aedf6877722f33f5387f251a6
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-01-27 15:26:28 +1100

    Refactor

--- src-tauri/src/filter/parser/blocks.rs
+++ src-tauri/src/filter/parser/blocks.rs
@@ -57,15 +57,14 @@ mod tests {
     fn test_parse_basic_blocks() {
         let content: Vec<String> = vec![
             "Show",
-            "    BaseType == \"Mirror of Kalandra\"",
-            "    SetFontSize 45",
-            "",
-            " Hide",
+            "BaseType == \"Mirror of Kalandra\"",
+            "SetFontSize 45",
+            "Hide",
             "BaseType == \"Scroll of Wisdom\"",
-            "      SetFontSize 18",
+            "SetFontSize 18",
             "Show",
-            "    Class \"Currency\"",
-            "    SetFontSize 40",
+            "Class \"Currency\"",
+            "SetFontSize 40",
         ]
         .into_iter()
         .map(Into::into)
@@ -78,67 +77,31 @@ mod tests {
         // First block
         assert!(blocks[0].is_show);
         assert_eq!(blocks[0].position.start_line, 0);
-        assert_eq!(blocks[0].position.end_line, 3);
+        assert_eq!(blocks[0].position.end_line, 2);
 
-        let expected_text = "Show\n    BaseType == \"Mirror of Kalandra\"\n    SetFontSize 45\n";
+        let expected_text = "Show\nBaseType == \"Mirror of Kalandra\"\nSetFontSize 45";
         assert_eq!(blocks[0].text, expected_text);
 
         // Second block
         assert!(!blocks[1].is_show);
-        assert_eq!(blocks[1].position.start_line, 4);
-        assert_eq!(blocks[1].position.end_line, 6);
+        assert_eq!(blocks[1].position.start_line, 3);
+        assert_eq!(blocks[1].position.end_line, 5);
 
-        let expected_text = " Hide\nBaseType == \"Scroll of Wisdom\"\n      SetFontSize 18";
+        let expected_text = "Hide\nBaseType == \"Scroll of Wisdom\"\nSetFontSize 18";
         assert_eq!(blocks[1].text, expected_text);
 
         // Third block
         assert!(blocks[2].is_show);
-        assert_eq!(blocks[2].position.start_line, 7);
-        assert_eq!(blocks[2].position.end_line, 9);
+        assert_eq!(blocks[2].position.start_line, 6);
+        assert_eq!(blocks[2].position.end_line, 8);
 
-        let expected_text = "Show\n    Class \"Currency\"\n    SetFontSize 40";
+        let expected_text = "Show\nClass \"Currency\"\nSetFontSize 40";
         assert_eq!(blocks[2].text, expected_text);
     }
 
     #[test]
-    fn test_parse_with_comments() {
-        let content: Vec<String> = vec![
-            "# Comment at start",
-            "Show # Comment after Show",
-            "    BaseType == \"Mirror\" # Comment after rule",
-            "",
-            "# Comment between blocks",
-            "Hide",
-            "    BaseType == \"Wisdom\"",
-        ]
-        .into_iter()
-        .map(Into::into)
-        .collect();
-
-        let blocks = parse_blocks(&content).unwrap();
-
-        assert_eq!(blocks.len(), 2);
-
-        // First block
-        assert!(blocks[0].is_show);
-        assert_eq!(blocks[0].position.start_line, 1);
-        assert_eq!(blocks[0].position.end_line, 4);
-
-        let expected_text = "Show # Comment after Show\n    BaseType == \"Mirror\" # Comment after rule\n\n# Comment between blocks";
-        assert_eq!(blocks[0].text, expected_text);
-
-        // Second block
-        assert!(!blocks[1].is_show);
-        assert_eq!(blocks[1].position.start_line, 5);
-        assert_eq!(blocks[1].position.end_line, 6);
-
-        let expected_text = "Hide\n    BaseType == \"Wisdom\"";
-        assert_eq!(blocks[1].text, expected_text);
-    }
-
-    #[test]
     fn test_single_block() {
-        let content: Vec<String> = vec!["Show", "    BaseType == \"Mirror\""]
+        let content: Vec<String> = vec!["Show", "BaseType == \"Mirror\""]
             .into_iter()
             .map(Into::into)
             .collect();
@@ -150,36 +113,25 @@ mod tests {
         assert_eq!(blocks[0].position.start_line, 0);
         assert_eq!(blocks[0].position.end_line, 1);
 
-        let expected_text = "Show\n    BaseType == \"Mirror\"";
-        assert_eq!(blocks[0].text, expected_text);
-    }
-
-    #[test]
-    fn test_tabs() {
-        let content: Vec<String> = vec!["", "\t\t Hide", "\t\tBaseType == \"Mirror\""]
-            .into_iter()
-            .map(Into::into)
-            .collect();
-
-        let blocks = parse_blocks(&content).unwrap();
-
-        assert_eq!(blocks.len(), 1);
-        assert!(!blocks[0].is_show);
-        assert_eq!(blocks[0].position.start_line, 1);
-        assert_eq!(blocks[0].position.end_line, 2);
-
-        let expected_text = "\t\t Hide\n\t\tBaseType == \"Mirror\"";
+        let expected_text = "Show\nBaseType == \"Mirror\"";
         assert_eq!(blocks[0].text, expected_text);
     }
 
     #[test]
     fn test_no_blocks() {
-        let content: Vec<String> = vec!["", "    nothing to see here", "    "]
+        let content: Vec<String> = vec!["nothing to see here", "another one"]
             .into_iter()
             .map(Into::into)
             .collect();
 
         let blocks = parse_blocks(&content).unwrap();
+
+        assert_eq!(blocks.len(), 0);
+    }
+
+    #[test]
+    fn test_empty_content() {
+        let blocks = parse_blocks(&[]).unwrap();
 
         assert_eq!(blocks.len(), 0);
     }
