42337647f7d74e0e1cd0db2a69fb63b039948d05
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-01-27 15:48:19 +1100

    Refactor

--- src-tauri/src/filter/parser/lines.rs
+++ src-tauri/src/filter/parser/lines.rs
@@ -1,9 +1,13 @@
 use super::remove_comment::remove_comment;
 use std::error::Error;
 
-/// Parses content of a Path of Exile loot filter file into a vector of lines.
+/// Parses content of a loot filter file into a vector of lines.
 pub fn content_to_lines(content: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    Ok(content.lines().map(|line| remove_comment(line)).collect())
+    Ok(content
+        .lines()
+        .map(|line| remove_comment(line).trim().to_string())
+        .filter(|line| !line.is_empty())
+        .collect())
 }
 
 #[cfg(test)]
@@ -25,10 +29,9 @@ BaseType "Scroll of Wisdom""#;
             lines,
             vec![
                 "Show",
-                "BaseType \"Mirror of Kalandra\" ",
-                "SetFontSize 45 ",
-                "",
-                "Hide ",
+                "BaseType \"Mirror of Kalandra\"",
+                "SetFontSize 45",
+                "Hide",
                 "BaseType \"Scroll of Wisdom\""
             ]
         );
@@ -39,5 +42,26 @@ BaseType "Scroll of Wisdom""#;
         let content = "";
         let lines = content_to_lines(content).unwrap();
         assert!(lines.is_empty());
+    }
+
+    #[test]
+    fn test_tabs_and_spaces() {
+        let content = "\t  Show  \t\n\t\tBaseType \"Mirror\"  \t";
+        let lines = content_to_lines(content).unwrap();
+        assert_eq!(lines, vec!["Show", "BaseType \"Mirror\""]);
+    }
+
+    #[test]
+    fn test_empty_lines() {
+        let content = r#"
+
+        Show
+
+  BaseType "Mirror"
+
+"#;
+
+        let lines = content_to_lines(content).unwrap();
+        assert_eq!(lines, vec!["Show", "BaseType \"Mirror\""]);
     }
 }
