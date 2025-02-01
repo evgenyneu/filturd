c794ed04f6b6281478ae8f190d11bc8c20b446e8
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-01 18:28:48 +1100

    Parse file

--- src-tauri/src/filter/parser/parse_file.rs
+++ src-tauri/src/filter/parser/parse_file.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 
+use crate::filter::parser::blocks::{Block, parse_block_with_lines};
+use crate::filter::parser::block_item::ParseError;
 use crate::filter::parser::blocks_with_lines::parse_lines;
-use crate::filter::parser::blocks_with_lines::BlockWithLines;
 use crate::filter::parser::lines::content_to_lines;
 use crate::filter::parser::read_from_disk::read_filter_from_disk;
 
@@ -11,10 +12,11 @@ use crate::filter::parser::read_from_disk::read_filter_from_disk;
 /// and then parses the remaining lines into blocks.
 ///
 /// Returns a vector of parsed BlockItem on success or an error if processing fails.
-pub async fn parse_file(path: &Path) -> Result<Vec<BlockWithLines>, Box<dyn std::error::Error>> {
+pub async fn parse_file(path: &Path) -> Result<Vec<Block>, ParseError> {
     let content = read_filter_from_disk(path).await?;
     let lines = content_to_lines(&content);
     let blocks_with_lines = parse_lines(&lines);
+    let blocks = parse_block_with_lines(&blocks_with_lines)?;
 
-    Ok(blocks_with_lines)
+    Ok(blocks)
 }
