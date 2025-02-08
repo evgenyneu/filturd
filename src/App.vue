33c42cfa10a264319e200f1d213e95907e67b361
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-08 19:22:48 +1100

    WOrk

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,9 @@ async function openFile() {
   <div class="h-screen flex flex-col bg-white dark:bg-gray-900">
     <nav
       class="p-4 bg-gray-100 dark:bg-gray-800 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
-      <button @click="openFile" class="rounded-lg border border-transparent px-4 py-2 text-base font-medium
+      <button @click="openFile" class="rounded-lg border px-4 py-2 text-base font-medium
                bg-white text-gray-900 dark:bg-poe-bg dark:text-poe-text-50
+               border-transparent dark:border-poe-border
                hover:border-poe-hover-600 hover:text-poe-hover-600
                dark:hover:border-poe-hover-300 dark:hover:text-poe-hover-300
                active:bg-gray-200 dark:active:bg-poe-border
@@ -44,8 +45,8 @@ async function openFile() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
           class="w-5 h-5">
           <path stroke-linecap="round" stroke-linejoin="round"
-            d="M3 7v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-6l-2-2H5a2 2 0 0 0-2 2z" />
-          <path stroke-linecap="round" stroke-linejoin="round" d="M9 14l3-3m0 0l3 3m-3-3v7" />
+            d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7l6 6v10a2 2 0 0 1-2 2z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v6h6 M8 11h8 M8 15h8" />
         </svg>
         Open
       </button>
