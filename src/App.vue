878f065945895fd017904499b8a5da029bd1ac43
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-08 21:08:05 +1100

    Styling

--- src/App.vue
+++ src/App.vue
@@ -79,16 +79,21 @@ async function openFile() {
         Open Filter
       </button>
 
-      <button @click="updateTheme(theme === 'dark' ? 'light' : 'dark')"
-        class="p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700"
-        :title="theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'">
-        <svg v-if="theme === 'dark'" xmlns="http://www.w3.org/2000/svg" class="size-5 text-gray-700 dark:text-gray-300"
-          fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <button @click="updateTheme(theme === 'dark' ? 'light' : 'dark')" class="rounded-lg border p-2 text-base font-medium
+               bg-white text-gray-900 dark:bg-poe-bg dark:text-poe-text-50
+               border-transparent dark:border-poe-border
+               hover:border-poe-hover-600 hover:text-poe-hover-600
+               dark:hover:border-poe-hover-300 dark:hover:text-poe-hover-300
+               active:bg-gray-200 dark:active:bg-poe-border
+               shadow-sm cursor-pointer transition-colors duration-250
+               focus:outline-none" :title="theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'">
+        <svg v-if="theme === 'dark'" xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24"
+          stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
-        <svg v-else xmlns="http://www.w3.org/2000/svg" class="size-5 text-gray-700 dark:text-gray-300" fill="none"
-          viewBox="0 0 24 24" stroke="currentColor">
+        <svg v-else xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24"
+          stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
