b4d47cdb97672ddb25b339c62c60891b66f1c8b4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-09 09:13:55 +1100

    Add File menu with exit

--- src/composables/useAppMenu.ts
+++ src/composables/useAppMenu.ts
@@ -1,22 +1,28 @@
 import { Menu } from "@tauri-apps/api/menu";
 import { onMounted } from "vue";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 export function useAppMenu() {
   async function initializeMenu() {
     const menu = await Menu.new({
       items: [
         {
-          id: "quit",
-          text: "Quit",
-          action: () => {
-            console.log("quit pressed");
-          },
+          id: "file",
+          text: "File",
+          items: [
+            {
+              id: "exit",
+              text: "Exit",
+              action: async () => {
+                await getCurrentWindow().close();
+              },
+            },
+          ],
         },
       ],
     });
 
     await menu.setAsAppMenu();
-    console.log("-------------menu loaded-------------");
   }
 
   onMounted(() => {
