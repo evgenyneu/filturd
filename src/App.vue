e6ea125f786f20882d63d6c77bc5e6be9a126b3f
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-08 19:17:27 +1100

    WOrk

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,8 @@ async function openFile() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
           class="w-5 h-5">
           <path stroke-linecap="round" stroke-linejoin="round"
-            d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+            d="M3 7v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-6l-2-2H5a2 2 0 0 0-2 2z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 14l3-3m0 0l3 3m-3-3v7" />
         </svg>
         Open
       </button>
