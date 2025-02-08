9a85ea55316859cb5477ae205f6c301954e78e8a
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-08 17:00:03 +1100

    Add poe font

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,12 @@ async function openFile() {
   <div class="h-screen flex flex-col bg-white dark:bg-gray-900">
     <nav
       class="p-4 bg-gray-100 dark:bg-gray-800 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
-      <button @click="openFile"
-        class="rounded-lg border border-transparent px-4 py-2 text-base font-medium text-gray-900 bg-white shadow-sm cursor-pointer transition-colors duration-250 hover:border-blue-600 active:bg-gray-200 focus:outline-none dark:text-white dark:bg-gray-800 dark:hover:border-blue-400 dark:active:bg-gray-700 flex items-center gap-2">
+      <button @click="openFile" class="rounded-lg border border-transparent px-4 py-2 text-base font-medium
+               bg-white text-gray-900 dark:bg-poe-bg dark:text-poe-text
+               hover:border-poe-hover hover:text-poe-hover
+               active:bg-gray-200 dark:active:bg-poe-border
+               shadow-sm cursor-pointer transition-colors duration-250
+               focus:outline-none flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
           class="w-5 h-5">
           <path stroke-linecap="round" stroke-linejoin="round"
