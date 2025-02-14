a93450c860b98bc21e3533f9c98a8e94ce9667c4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-14 05:29:47 +0000

    Filter example

--- src-tauri/src/commands/open_file.rs
+++ src-tauri/src/commands/open_file.rs
@@ -1,6 +1,8 @@
 use crate::filter::parser::parse_file::parse_file;
 use crate::filter::parser::blocks::Block;
 use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
 #[tauri::command]
 pub async fn open_file(path: &str) -> Result<Vec<Block>, ()> {
@@ -10,5 +12,9 @@ pub async fn open_file(path: &str) -> Result<Vec<Block>, ()> {
     println!("parse_file() took {:?}", duration);
     assert!(result.is_ok());
     let blocks = result.unwrap();
+    let json: String = serde_json::to_string(&blocks).unwrap();
+    // Save to file
+    let mut file = File::create("blocks.json").unwrap();
+    file.write_all(json.as_bytes()).unwrap();
     Ok(blocks)
 }
