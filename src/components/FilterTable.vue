71743530e5e13ce4dcb494993d4a069e77e3cb57
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-14 07:52:11 +0000

    Show table

--- src/components/FilterTable.vue
+++ src/components/FilterTable.vue
@@ -7,17 +7,19 @@ defineProps<{
 </script>
 
 <template>
-  <table class="w-full border-collapse">
+  <table class="w-full border-collapse bg-white/5 dark:bg-poe-bg/20">
     <thead>
-      <tr class="bg-gray-100 dark:bg-gray-800">
-        <th class="p-2 text-left">#</th>
-        <th class="p-2 text-left">Type</th>
+      <tr class="border-b border-gray-200 dark:border-poe-border">
+        <th class="w-fit whitespace-nowrap py-2 px-1 text-left font-normal text-gray-600 dark:text-poe-text-900">#</th>
+        <th class="w-full py-2 px-1 text-left font-normal text-gray-600 dark:text-poe-text-900">Type</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="block in blocks" :key="block.order" class="border-t border-gray-200 dark:border-gray-700">
-        <td class="p-2">{{ block.order }}</td>
-        <td class="p-2">{{ block.name }}</td>
+      <tr v-for="block in blocks" :key="block.order" class="border-b border-gray-200/50 dark:border-poe-border/50
+                 hover:bg-gray-50 dark:hover:bg-poe-border/30
+                 transition-colors duration-150">
+        <td class="py-2 px-1 font-medium text-gray-900 dark:text-poe-text-400">{{ block.order }}</td>
+        <td class="py-2 px-1 font-medium text-gray-900 dark:text-poe-text-400">{{ block.name }}</td>
       </tr>
     </tbody>
   </table>
