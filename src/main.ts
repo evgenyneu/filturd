9e6d4e2517a581949654e07c443eae4ac2bd7cc4
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-02-16 08:32:51 +1100

    Style tooltip

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,18 @@ import "./styles/main.css";
 import App from "./App.vue";
 import FloatingVue from "floating-vue";
 
-createApp(App).use(FloatingVue).mount("#app");
+let app = createApp(App);
+
+app.use(FloatingVue, {
+  distance: 10,
+  themes: {
+    tooltip: {
+      delay: {
+        show: 0,
+        hide: 0,
+      },
+    },
+  },
+});
+
+app.mount("#app");
